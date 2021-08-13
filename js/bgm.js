04f0d80e5cb9877d23cf1defd0312115dd88d1ed
yejin0528 <74442264+yejin0528@users.noreply.github.com>
2021-08-13 14:57:10 +0900

    Update BGM
    BGM 완성

--- js/bgm.js
+++ js/bgm.js
@@ -1,8 +1,4 @@
 const bgmBtn = document.querySelector(".bgm__btn");
-const bgmIcon = document.querySelector("#bgm_icon");
-
-const PLAY_ICON = "fa-play-circle";
-const PAUSE_ICON = "fa-pause-circle";
 
 let bgmPlaying = false;
 const bgm = new Audio("bgm/todo_bgm.mp3"); //Audio 객체 생성
@@ -11,15 +7,12 @@ function bgmPlay() {
     if (bgmPlaying == false) { //만약 재생 중 아니면
         bgmPlaying = true;
         bgm.play(); //재생
+        bgmBtn.innerHTML = '<i id="bgm__icon" class="far fa-pause-circle"></i>'; //icon 변경
 
-        bgmIcon.classList.remove(PLAY_ICON);
-        bgmIcon.classList.add(PAUSE_ICON);
     } else {
         bgmPlaying = false;
         bgm.pause(); //일시정지
-
-        bgmIcon.classList.add(PLAY_ICON);
-        bgmIcon.classList.remove(PAUSE_ICON);
+        bgmBtn.innerHTML = '<i id="bgm__icon" class="far fa-play-circle"></i>';
     }
 }
 
